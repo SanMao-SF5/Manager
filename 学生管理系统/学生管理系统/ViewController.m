487469ad255da4ce20ff1b2a73ec4a1f61a916d7
shenfangwu <shenfangwu@163.com>
2015-12-09 23:39:09 +0800

    创建了项目，配置了支持设备版本8.0以上仅支持横屏

---
+++ 学生管理系统/学生管理系统/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  学生管理系统
+//
+//  Created by 沈方武 on 15/12/9.
+//  Copyright © 2015年 SanMao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 学生管理系统/学生管理系统/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  学生管理系统
+//
+//  Created by 沈方武 on 15/12/9.
+//  Copyright © 2015年 SanMao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
